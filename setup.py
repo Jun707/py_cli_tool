4adcd77dbac1a2502376315bf9cff57b4f3685f7
Jun707 <juntowork@gmail.com>
2024-08-11 19:56:12 -0600

    added more info to setup

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='miso',
     version='0.0.2',
+    description= "a cli tool to help optimized workflow by saving frequently used url paths, can also group different urls",
+    author= "Junwen Huang",
+    author_email= "juntowork@gmail.com",
     packages=find_packages(),
     entry_points= {
         'console_scripts': [
