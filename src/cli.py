9a40ddea0c3e4d688e3999b82a76a5398e8e64ed
Jun707 <juntowork@gmail.com>
2024-08-12 19:37:32 -0600

    change user story, added arg for read subparser

--- src/cli.py
+++ src/cli.py
@@ -13,6 +13,7 @@ def parser_arguments():
 
     # reading url paths
     read_parser = subparser.add_parser("read", help = "open all the stored url paths")
+    read_parser.add_argument("-ls", "--list", action ="store_true")
     read_parser.set_defaults(func=read_files_url)
 
     # storing url paths
@@ -28,19 +29,20 @@ def parser_arguments():
     args = parser.parse_args()
     return args
 
-def read_files_url(args):
+def read_files_url(args, list = False):
     urls = []
 
     script_dir = os.path.dirname(os.path.realpath(__file__))
     file_path = os.path.join(script_dir, "url_storage.txt")
-    
     with open(file_path, "r") as file:
         for line in file:
             urls.append(line.strip())
-    for url in urls:
-        webbrowser.open(url)
-
-    print(urls)
+    if args.list:
+        for url in urls:
+            print(url, end = " ")
+    else:
+        for url in urls:
+            webbrowser.open(url)
     
 
 def save_input_url(args):
--- src/main.py
+++ src/main.py
@@ -4,8 +4,6 @@ def main():
     args = cli.parser_arguments()
     if hasattr(args, 'func'):
         args.func(args)
-        
-    print("hello world")
 
 if __name__ == "__main__":
     main()
